f4a0a479e470b53887425c2282d819db17999e5b
Bram Borggreve <borggreve@gmail.com>
2018-05-09 00:37:05 -0500

    fix(admin-ui): correctly align avatar in header

--- libs/admin-ui/src/lib/components/page-title/page-title.component.ts
+++ libs/admin-ui/src/lib/components/page-title/page-title.component.ts
@@ -3,10 +3,14 @@ import { Component, Input } from '@angular/core'
 @Component({
   selector: 'ui-page-title',
   template: `
-    <h2>{{title}}</h2>
-    <ng-content></ng-content>
+    <mat-toolbar>
+      <mat-icon style="margin-right: 5px;" *ngIf="icon">{{icon}}</mat-icon>
+      {{title}}
+      <ng-content></ng-content>
+    </mat-toolbar>
   `,
 })
 export class PageTitleComponent {
   @Input() public title
+  @Input() public icon
 }
