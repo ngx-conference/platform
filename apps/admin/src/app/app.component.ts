d06bb935b48505a2cbbf07bc33c4c932802e039c
Bram Borggreve <borggreve@gmail.com>
2018-05-14 01:25:04 -0500

    feat(admin): show loading indicator in AppComponent

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core'
+import { NavigationEnd, Router } from '@angular/router'
+import { filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   template: `
+    <div *ngIf="loading">Loading...</div>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  public loading = true
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => (this.loading = false))
+  }
+}
